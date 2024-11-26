5602f6d2bb1053dbd41af6acc59453f8c1fb9871
Rickardssson <oskar.rickardsson12@gmail.com>
2024-11-26 13:48:28 +0100

    Add random direction and pause to enemy movement

--- Assets/Scripts/SCREnemyMovement.cs
+++ Assets/Scripts/SCREnemyMovement.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class SCREnemyMovement : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private float _minDirectionChangeTime = 1f;
+    [SerializeField] private float _maxDirectionChangeTime = 5f;
+    [SerializeField] private float _waitTime = 2f;
     
     private Rigidbody2D _rigidbody;
     private PlayerAwarenessController _playerAwarenessController;
-    /*private Vector2 _targetDirection;*/
+    
+    private Vector2 _randomDirection;
+    private float _changeDirectionCooldown;
+    private bool _isWaiting;
+    
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
@@ -18,24 +25,68 @@ public class SCREnemyMovement : MonoBehaviour
         {
             Debug.LogError("PlayerAwareness Controller not found");
         }
+
+        SetRandomDirection();
     }
     
     private void FixedUpdate()
     {
-        if (_playerAwarenessController && _playerAwarenessController.AwareOfPlayer)
+        HandleRandomDirectionChange();
+        if (_playerAwarenessController != null && _playerAwarenessController.AwareOfPlayer)
         {
             MoveTowardsPlayer();
         }
         else
         {
-            StopMovement();
+            HandleRandomDirectionChange();
         }
     }
-
+    
     private void MoveTowardsPlayer()
     {
         Vector2 directionToPlayer = _playerAwarenessController.DirectionToPlayer;
         _rigidbody.velocity = directionToPlayer * _speed;
+    }
+    
+    private void HandleRandomDirectionChange()
+    {
+        if (_isWaiting)
+        {
+            StopMovement();
+        }
+        else
+        {
+            _rigidbody.velocity = _randomDirection * _speed;
+            _changeDirectionCooldown -= Time.deltaTime;
+            
+            if (_changeDirectionCooldown <= 0)
+            {
+                StartCoroutine(WaitBeforeMovement());
+            }
+        }
+    }
+
+    private IEnumerator WaitBeforeMovement()
+    {
+        // Enter waiting state
+        _isWaiting = true;
+        StopMovement();
+        
+        // Waiting State
+        yield return new WaitForSeconds(_waitTime);
+        
+        // Exit waiting state
+        SetRandomDirection();
+        _isWaiting = false;
+    }
+
+    private void SetRandomDirection()
+    {
+        float randomAngle = Random.Range(0f, 360f);
+        _randomDirection = new Vector2(
+            Mathf.Cos(randomAngle * Mathf.Deg2Rad), Mathf.Sin(randomAngle * Mathf.Deg2Rad)
+            ).normalized;
+        _changeDirectionCooldown = Random.Range(_minDirectionChangeTime, _maxDirectionChangeTime);
     }
 
     private void StopMovement()
