99ce2ef7ab6bea6f6e123c827083ee6e69efc10c
desgign <a24hugga@student.his.se>
2024-11-27 12:50:55 +0100

    Turret added
    Added a turret that can fire a bullet at enemies

---
+++ Assets/Scripts/SCRTurret.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SCRTurret : MonoBehaviour
+{
+
+    [Header("References")] 
+    [SerializeField] private Transform turretRotationPoint;
+    [SerializeField] private LayerMask enemyMask;
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firingPoint;
+    
+    
+    [Header("Attribute")] 
+    [SerializeField] private float targetingRange = 5f;
+    [SerializeField] private float rotationspeed = 100f;
+    [SerializeField] private float fireRate = 1f; // times it fires per second
+
+    private Transform target;
+    private float timeUntilFire;
+
+    private void Update()
+    {
+        if (target == null)
+        {
+            FindTarget();
+            return;
+        }
+
+        if (!CheckTargetIsInRange())
+        {
+            target = null;
+        }
+        else
+        {
+            timeUntilFire += Time.deltaTime;
+
+            if (timeUntilFire >= 1f / fireRate)
+            {
+                Shoot();
+                timeUntilFire = 0f;
+            }
+        }
+
+        RotateTowardsTarget();
+    }
+
+    private void Shoot()
+    {
+        GameObject bulletObj = Instantiate(bulletPrefab, firingPoint.position, Quaternion.identity);
+        SCRBullet bulletScript = bulletObj.GetComponent<SCRBullet>();
+        bulletScript.SetTarget(target);
+        
+    }
+
+    private void FindTarget()
+    {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetingRange, (Vector2)
+            transform.position, 0f, enemyMask);
+        if (hits.Length > 0)
+        {
+            target = hits[0].transform;
+        }
+    }
+
+    private void RotateTowardsTarget()
+    {
+        float angle = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg;
+
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        turretRotationPoint.rotation = Quaternion.RotateTowards(turretRotationPoint.rotation, targetRotation, rotationspeed * Time.deltaTime);
+    }
+
+    private bool CheckTargetIsInRange()
+    {
+        return Vector2.Distance(transform.position, target.position) <= targetingRange;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Handles.color = Color.yellow;
+        Handles.DrawWireDisc(transform.position, transform.forward, targetingRange);
+    }
+}
