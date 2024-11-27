4ea33a65778f36ca234681cefa4b07c453b9b106
Rickardssson <oskar.rickardsson12@gmail.com>
2024-11-27 15:27:53 +0100

    add more optimized movement and change layering for enemy

---
+++ Assets/Scripts/SCREnemyMovement2.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SCREnemyMovement2 : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float screenBorder;
+    
+    private Rigidbody2D rigidbody;
+    private PlayerAwarenessController playerAwarenessController;
+    private Vector2 targetDirection;
+    private float changeDirectionCooldown;
+    private Camera camera;
+    
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        playerAwarenessController = GetComponent<PlayerAwarenessController>();
+        targetDirection = transform.up;
+        camera = Camera.main;
+    }
+    
+    void FixedUpdate()
+    {
+        UpdateTargetDirection();
+        RotateTowardsTarget();
+        SetVelocity();
+    }
+
+    private void UpdateTargetDirection()
+    {
+        HandleRandomDirectionChange();
+        HandlePlayerTargeting();
+        HandleEnemyOffScreen();
+    }
+    
+    private void HandleRandomDirectionChange()
+    {
+        changeDirectionCooldown -= Time.deltaTime;
+
+        if (changeDirectionCooldown <= 0)
+        {
+            float angleChange = Random.Range(-90f, 90f);
+            Quaternion rotation = Quaternion.AngleAxis(angleChange, transform.forward);
+            targetDirection = rotation * targetDirection;
+            
+            changeDirectionCooldown = Random.Range(1f, 5f);
+        }
+    }
+    
+    private void HandleEnemyOffScreen()
+    {
+        Vector2 screenPosition = camera.WorldToScreenPoint(transform.position);
+
+        if ((screenPosition.x < 0 && targetDirection.x < 0) || 
+            (screenPosition.x > camera.pixelWidth && targetDirection.x > 0))
+        {
+            targetDirection = new Vector2(-targetDirection.x, targetDirection.y);
+        }
+        
+        if ((screenPosition.y < 0 && rigidbody.velocity.y < 0) || 
+            (screenPosition.y > camera.pixelHeight && rigidbody.velocity.y > 0))
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, -rigidbody.velocity.y);
+        }
+    }
+
+    private void HandlePlayerTargeting()
+    {
+        if (playerAwarenessController.AwareOfPlayer)
+        {
+            targetDirection = playerAwarenessController.DirectionToPlayer;
+        }
+    }
+    
+    private void RotateTowardsTarget()
+    {
+        Quaternion targetRotation = Quaternion.LookRotation(transform.forward, targetDirection);
+        Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        
+        rigidbody.SetRotation(rotation);
+    }
+
+    private void SetVelocity()
+    {
+            rigidbody.velocity = transform.up * speed;
+    }
+}
