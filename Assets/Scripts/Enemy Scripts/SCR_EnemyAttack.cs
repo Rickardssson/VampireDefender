ae928b800abf918aa72410b2affcc21f9700e84f
desgign <a24hugga@student.his.se>
2024-12-04 15:15:05 +0100

    little cooldown for enemy

--- Assets/Scripts/Enemy Scripts/SCR_EnemyAttack.cs
+++ Assets/Scripts/Enemy Scripts/SCR_EnemyAttack.cs
@@ -1,19 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
+
 
 public class SCR_EnemyAttack : MonoBehaviour
 {
     [SerializeField] private float damageAmount;
+    [SerializeField] private float attackRate = 1f;
 
+    private float timeUntilAttack;
+
+
+    private void Update()
+    {
+        timeUntilAttack += Time.deltaTime;
+    }
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.gameObject.GetComponent<TopDownMovement>())
+        if (timeUntilAttack >= 1f/attackRate && collision.gameObject.GetComponent<TopDownMovement>())
         {
             var playerHealth = collision.gameObject.GetComponent<SCR_PlayerHealth>();
             Debug.Log("I attacked you foul vampire!");
+            timeUntilAttack = 0f;
             
             playerHealth.TakeDamage(damageAmount);
         }
+        else return;
     }
 }
