04aae022d2a3a53557b5a0854c6c7d538ddfb4f0
SzariesIV <szentax@gmail.com>
2024-12-05 12:18:04 +0100

    add Blood to HUD

--- Assets/Scripts/Player Scripts/SCR_PlayerHUD.cs
+++ Assets/Scripts/Player Scripts/SCR_PlayerHUD.cs
@@ -7,25 +7,36 @@ using TMPro;
 
 public class SCR_PlayerHUD : MonoBehaviour
 {
-   [SerializeField] private TextMeshProUGUI healthText;
+   [SerializeField] private TextMeshProUGUI healthText, bloodText;
+   [SerializeField] private SCR_BuildingManager buildingManager;
    private GameObject player;
    private float playerRemHealth;
+   private float playerRemBlood;
+   
+   private SCR_PlayerHealth _playerHealth;
 
    private void Start()
    {
       player = GameObject.FindGameObjectWithTag("Player");
+      _playerHealth = player.GetComponent<SCR_PlayerHealth>();
       
    }
 
    private void Update()
    {
-      playerRemHealth = player.GetComponent<SCR_PlayerHealth>().RemainingHealthPercentage;
-      SetText(playerRemHealth);
+      playerRemHealth = _playerHealth.RemainingHealthPercentage; 
+      playerRemBlood =  buildingManager.currency;
+      SetTextHealth(playerRemHealth);
+      SetTextBlood(playerRemBlood);
    }
 
-   private void SetText(float value)
+   private void SetTextHealth(float value)
    {
       float valuePercent = value * 100;
       healthText.SetText($"{(int)valuePercent}/100");
+   }
+   private void SetTextBlood(float value)
+   {
+      bloodText.SetText($"Blood: {(int)value}");
    }
 }
