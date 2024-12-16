eecf580fbaa42afef2192b092e56f3b2ac3394e9
SzariesIV <szentax@gmail.com>
2024-12-16 21:42:40 +0100

    preparations for introcutscene

---
+++ Assets/Scripts/Intro Scene Scripts/SCR_CutSceneManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_CutSceneManager : MonoBehaviour
+{
+    private float guiAlpha;
+    void Start()
+    {
+        PlayerPrefs.SetString("HasSeenIntro", "yes");
+        GUIFade(0, 1, 1); // Start, end, length in seconds
+    }
+    
+
+    
+
+    void OnGUI () {
+        GUI.color.a = guiAlpha;
+        if (GUILayout.Button("Click me to fade out")) {
+            GUIFade(1, 0, 2);
+        }
+    }
+
+    void GUIFade (float start, float end, float length) { 
+        for (i = 0.0; i <= 1.0; i += Time.deltaTime*(1/length)) { 
+            guiAlpha = Mathf.Lerp(start, end, i); 
+            return; 
+        }
+        guiAlpha = end; // Accounts for Time.deltaTime variance
+    }
+}
