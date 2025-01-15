e8d0bbd63e04e4844e3a0c0b7d896903fb110006
Rickardssson <oskar.rickardsson12@gmail.com>
2025-01-15 17:13:59 +0100

    Update sprites for clock and AOE

--- Assets/SCR_DayNightClock.cs
+++ Assets/SCR_DayNightClock.cs
@@ -1,11 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SCR_DayNightClock : MonoBehaviour
 {
+    [SerializeField] private float StartRotation = 0;
     private float rotation;
     private RectTransform rectTransform;
+
+    private void Start()
+    {
+        rotation = StartRotation;
+        GetRotationComponent();
+    }
 
     // Update is called once per frame
     void Update()
